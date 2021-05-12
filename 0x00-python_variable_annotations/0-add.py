8cc0c67db04c6ce58e9aaa99b84b72467bb81732
m5235 <m.ladhari@outlook.com>
2021-05-12 17:34:39 +0100

    fisrt commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" a type-annotated function add """
+
+
+def add(a: float, b: float) -> float:
+    """
+    a type-annotated function add that takes
+    a float a and a float b as arguments
+    """
+    return a + b
