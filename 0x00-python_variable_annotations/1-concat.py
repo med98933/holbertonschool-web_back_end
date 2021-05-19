8ae31ee612202904629508f8d884625f50ed1f0b
m5235 <m.ladhari@outlook.com>
2021-05-19 08:27:39 +0100

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" a type-annotated function concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+ function to concat two strings
+    """
+    return str1 + str2
+    
