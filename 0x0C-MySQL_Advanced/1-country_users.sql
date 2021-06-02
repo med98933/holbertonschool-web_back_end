497bd5842a426c0699f336c4d2c832583e7d1e85
momo98-sys <m.ladhari@outlook.com>
2021-06-02 20:55:03 +0100

    task1

--- 0x0C-MySQL_Advanced/1-country_users.sql
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -1,4 +1,4 @@
--- creates a table users
+--  SQL script that creates a table users
 -- script can be executed on any database
 CREATE TABLE IF NOT EXISTS `users` (  
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
