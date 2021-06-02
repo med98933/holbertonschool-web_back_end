b03bbf40039ff0f7261d19e7d35195f083137b53
momo98-sys <m.ladhari@outlook.com>
2021-06-02 15:39:09 +0100

    add

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 """ Regex-ing """
-import re
 from typing import List
-
+import re
 
 
 def filter_datum(fields: List[str],
                  redaction: str,
                  message: str,
                  separator: str) -> str:
-    """ function called filter_datum that returns the log message obfuscated """
+    """ A function that returns the log message obfuscated """
     for i in fields:
         message = re.sub(fr'{i}=.+?{separator}',
                          f'{i}={redaction}{separator}', message)
