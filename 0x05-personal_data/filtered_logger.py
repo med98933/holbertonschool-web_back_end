f5fb7222757e3b514f04c0c2d64a4cc32bc2208b
momo98-sys <m.ladhari@outlook.com>
2021-06-02 16:49:28 +0100

    task1

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python3
-""" Regex-ing """
+""" Regex-ing, Log formatter """
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ constructor """
+
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """  generates a log """
+        msg = logging.Formatter(self.FORMAT).format(record)
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
 
 
 def filter_datum(fields: List[str],
