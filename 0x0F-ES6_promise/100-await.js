5ea9f950db56c6a2d31204beca18c0a961e65a7c
momo98-sys <m.ladhari@outloo.com>
2021-06-15 16:53:34 +0100

    add

---
+++ 0x0F-ES6_promise/100-await.js
@@ -0,0 +1,15 @@
+// Await and async
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let photoval;
+  let userval;
+  try {
+    photoval = await uploadPhoto();
+    userval = await createUser();
+  } catch (e) {
+    photoval = null;
+    userval = null;
+  }
+  return { photo: photoval, user: userval };
+}
---
+++ 0x0F-ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+// jueue push
+export default function guardrail(mathFunction) {
+    const j = [];
+  
+    try {
+      j.push(mathFunction());
+    } catch (e) {
+      j.push(e.toString());
+    }
+    j.push('Guardrail was processed');
+  
+    return j;
+  }
