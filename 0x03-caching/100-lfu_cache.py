460c1078166dbbf9792d86ccd4a3ae8e0bf44671
momo98-sys <m.ladhari@outloo.com>
2021-06-16 23:07:36 +0100

    adv

--- 0x03-caching/100-lfu_cache.py
+++ 0x03-caching/100-lfu_cache.py
@@ -1,52 +1,61 @@
 #!/usr/bin/python3
-""" 100-lfu_cache.py"""
-
+""" LFUCache
+"""
+from datetime import datetime
+from collections import defaultdict
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """
-    class LFUCache that inherits from BaseCaching and is a caching system
-    """
-
-    def __init__(s):
-        """Initiliaze """
+    """LFUCache Class """
+    def __init__(self):
+        """Initiliaze"""
         super().__init__()
-        s.lfu_order = []
-        s.frequency = {}
+        self.cache_by_time = {}
+        self.cache_by_frequency_use = defaultdict(int)
 
-    def put(s, k, c):
-        """
-Add an c in the cache
-        """
-        if k is not None and c is not None:
-            if k in s.cache_data:
-                s.cache_data[k] = c
-                s.frequency[k] += 1
-                s.lfu_order.remove(k)
-            else:
-                if len(s.cache_data) >= s.MAX_cS:
-                    min_value = min(s.frequency.values())
-                    min_ks = [k for k in s.frequency
-                            if s.frequency[k] == min_value]
-                    for x in range(len(s.lfu_order)):
-                        if s.lfu_order[x] in min_ks:
-                            break
-                    del s.cache_data[s.lfu_order[x]]
-                    del s.frequency[s.lfu_order[x]]
-                    print("DISCARD:", s.lfu_order[x])
-                    s.lfu_order.pop(x)
-                s.cache_data[k] = c
-                s.frequency[k] = 1
-            s.lfu_order.append(k)
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data"""
+        if key and item:
+            self.cache_by_time[key] = datetime.now()
+            self.cache_data[key] = item
+            self.cache_by_frequency_use[key] += 1
 
-    def get(s, k):
-        """
-        Get an c by k
-        """
-        if k in s.cache_data:
-            s.lfu_order.remove(k)
-            s.lfu_order.append(k)
-            s.frequency[k] += 1
-            return s.cache_data[k]
-        return None
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+
+                frequency_use_filtered = {}
+                for k, v in self.cache_by_frequency_use.items():
+                    if k != key:
+                        frequency_use_filtered[k] = v
+                keys_by_frequency_used = sorted(frequency_use_filtered,
+                                                key=frequency_use_filtered.get)
+                key_to_delete = keys_by_frequency_used[0]
+
+                count = frequency_use_filtered[key_to_delete]
+                posibles_es_to_discard_dict = {}
+                for k, v in frequency_use_filtered.items():
+                    if v == count:
+                        posibles_es_to_discard_dict[k] = v
+                if len(posibles_es_to_discard_dict) > 1:
+                    es_to_discard_by_time = {}
+                    for k, v in self.cache_by_time.items():
+                        if k in posibles_es_to_discard_dict.keys():
+                            es_to_discard_by_time[k] = v
+
+                    es_by_time = sorted(
+                                          es_to_discard_by_time,
+                                          key=es_to_discard_by_time.get)
+                    key_to_delete = es_by_time[0]
+
+                del self.cache_by_time[key_to_delete]
+                del self.cache_data[key_to_delete]
+                del self.cache_by_frequency_use[key_to_delete]
+                print('DISCARD: {}'.format(key_to_delete))
+
+    def get(self, key):
+        """return the value of key in self.cache_data"""
+        e = self.cache_data.get(key)
+        if e:
+            self.cache_by_time[key] = datetime.now()
+            self.cache_by_frequency_use[key] += 1
+        return e
