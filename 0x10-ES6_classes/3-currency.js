2274988756c7cb3f2abd4e23fbf3c22bdbfe9521
momo98-sys <m.ladhari@outloo.com>
2021-06-17 11:16:08 +0100

    fix

--- 0x10-ES6_classes/3-currency.js
+++ 0x10-ES6_classes/3-currency.js
@@ -1,32 +1,32 @@
 export default class Currency {
-    constructor(code, name) {
-      if (typeof code === 'string' && typeof name === 'string') {
-        this._code = code;
-        this._name = name;
-      } else {
-        throw (TypeError('Attributes must be strings'));
-      }
+  constructor(code, name) {
+    if (typeof code === 'string' && typeof name === 'string') {
+      this._code = code;
+      this._name = name;
+    } else {
+      throw (TypeError('Attributes must be strings'));
     }
-  
-    get name() {
-      return this._name;
-    }
-  
-    get code() {
-      return this._code;
-    }
-  
-    set name(newName) {
-        if (typeof newName !== 'string') throw TypeError('name must be a String');
-        this._name = newName;
-      }
-  
-    set code(newCode) {
-        if (typeof newCode !== 'string') throw TypeError('code must be a String');
-        this._code = newCode;
-      }
-  
-    displayFullCurrency() {
-        return `${this.name} (${this.code})`;
-      }
-    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') throw TypeError('name must be a String');
+    this._name = newName;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') throw TypeError('code must be a String');
+    this._code = newCode;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
