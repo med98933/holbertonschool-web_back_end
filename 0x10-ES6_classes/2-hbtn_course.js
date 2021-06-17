8d80bb7472ecd9c2b12de3bc44fd18b0f248d853
momo98-sys <m.ladhari@outloo.com>
2021-06-17 11:03:00 +0100

    add

---
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,58 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.constructor.verify(name, length, students);
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  static verify(name = null, length = null, students = null) {
+    if (name !== null && typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    if (length !== null && typeof length !== 'number') {
+      throw TypeError('Length must be a string');
+    }
+    if (students !== null && !(Array.isArray(students))) {
+      throw TypeError('Students must be an array of strings');
+    }
+    if (students !== null) {
+      for (const x of students) {
+        if (typeof x !== 'string') {
+          throw TypeError('Students must be an array of strings');
+        }
+      }
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  set students(students) {
+    this.constructor.verify(null, null, students);
+    this._students = students;
+  }
+}
