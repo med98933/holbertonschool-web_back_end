69faaca366715517db4e3a94ac1050c34e50d7c7
momo98-sys <m.ladhari@outloo.com>
2021-06-17 11:49:31 +0100

    add

---
+++ 0x10-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,10 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    return hint === 'string' ? this._location : this._size;
+  }
+}
