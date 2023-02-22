849f6699084c230a294fe1377ff985382bc58582
med98933 <m.ladhari@outlook.com>
2023-02-22 23:33:14 +0100

    done

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,15 +1,13 @@
-process.stdin.setEncoding('utf8');
+process.stdin.setEncoding('utf-8');
 
-console.log('Welcome to Holberton School, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-  let name = process.stdin.read();
+  const name = process.stdin.read();
   if (name !== null) {
-    name = name.trim();
-    console.log(`Your name is: ${name}`);
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
-
 process.stdin.on('end', () => {
-  console.log('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
